0392d1cb34af0368cb76b457b602b4c3a0feeffb
KaHei Ng <kaheicanaan@gmail.com>
2020-07-09 21:10:42 +0800

    Add get_sensor_readings

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
-import os
 import subprocess
 
+import psutil
 
-def get_thermal_info():
+
+# =====================
+# || Sensor readings ||
+# =====================
+
+def _get_thermal_info():
     """Get outputs from 'powermetrics' command, return outputs line by line as a generator.
 
     Returns:
@@ -14,6 +19,34 @@ def get_thermal_info():
     output = proc.communicate()[0].decode()
     print(output)
 
+    thermal_info = dict()
+    for line in output.split('\n'):
+        if line.startswith('CPU die temperature:'):
+            thermal_info.update({
+                'cpu_temperature': float(line[21:26])
+            })
+        elif line.startswith('GPU die temperature:'):
+            thermal_info.update({
+                'gpu_temperature': float(line[21:26])
+            })
+        elif line.startswith('Fan:'):
+            thermal_info.update({
+                'fan_speed': int(line[5:9])
+            })
+
+    return thermal_info
+
+
+def _get_cpu_load():
+    return {'cpu_load': psutil.cpu_percent()}
+
+
+def get_sensor_readings():
+    readings = dict()
+    readings.update(_get_thermal_info())
+    readings.update(_get_cpu_load())
+    return readings
+
 
 if __name__ == '__main__':
-    get_thermal_info()
+    sensor_readings = get_sensor_readings()
